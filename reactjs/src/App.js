9a335b15ede2370a10b5790f5887f7d324dfe2c7
Aadarsh <aadarshingh@hello.com>
2021-07-07 15:30:35 +0530

    react-basic-file

---
+++ reactjs/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello world Aadarsh singh
+        </p>
+        <p>
+          what is working
+        </p>
+          what is google
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
