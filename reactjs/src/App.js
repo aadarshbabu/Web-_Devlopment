a3ac83ad019a2324fbfc713399c94220016b526f
Aadarsh <aadarshingh@hello.com>
2021-07-08 14:14:14 +0530

    some change

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,28 +1,32 @@
-import logo from './logo.svg';
+// import Header from './Header'; // if you want to access or call any functional component which is present in another file. first import the  Header.
+ //.. if any component file is default exported. You can import in any name.
+// import {Sidebar} from "./Header" // when you are export the specific funcional component. import the component in {Component Name} this way.
+import React from 'react';
 import './App.css';
+// Ensure that your functional component First charctore is Captioal later
+function Data() {
+    return (
+      <div>
+      <h2>Welcome to Data component </h2>
+    
+      </div>
+    );
+    
+}
 
 function App() {
+  let name= "aadarsh";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello world Aadarsh singh
-        </p>
-        <p>
-          what is working
-        </p>
-          what is google
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>{name}</h1>
+      
+      <Data />
+    
+      {/* <Header />
+      <Sidebar /> */}
     </div>
+    
   );
 }
 
---
+++ reactjs/src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+     function Header() {
+        return (
+            <div>
+             <h2>  Welcome to Header component in Another File </h2> 
+            </div> 
+        );
+    }
+
+   export function Sidebar(){ // it is a example in specafic functional component exporting.
+        return (
+            <div>
+                <h1>Welcome to the sidebar component in Header.js File</h1>
+            </div>
+        );
+
+    }
+
+   export default Header; // if you want to access the any component in another to first export the component. then call the component in another file.
+        // export is nessacery if component in define in another file.
