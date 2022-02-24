92a8684a0c12dacae0e390cae4f73a35297c7f86
aadarsh <aadarshsingh121@gmail.com>
2022-02-24 10:37:10 +0530

    some buggy

--- ExpenceManager/Backend/db/conn.js
+++ ExpenceManager/Backend/db/conn.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 
 async function connection() {
 
-<<<<<<< HEAD:ExpenceManager/Backend/db/conn.js
     const con = await mongoose.connect(process.env.DBCONFIG);
-=======
-    const con = await mongoose.connect("y");
->>>>>>> 08a471ff97ecde581849e9e363a6125ccc601f23:ExpenceManager/db/conn.js
 
     if (con) {
         console.log("connected");
